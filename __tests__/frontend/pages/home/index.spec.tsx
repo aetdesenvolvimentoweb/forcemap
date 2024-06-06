70f7873e83e16fdf60daef150c83996883d93805
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-06-06 14:46:26 -0300

    test: ensure that homepage render header, main and footer

--- __tests__/frontend/pages/home/index.spec.tsx
+++ __tests__/frontend/pages/home/index.spec.tsx
@@ -3,9 +3,19 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 
 describe("HomePage", () => {
-  test("should be able to render a heading", () => {
+  test("should be able to render a header", () => {
     render(<HomePage />);
 
-    expect(screen.getByText("ForceMap")).toBeDefined();
+    expect(screen.getAllByText("ForceMap")).toBeDefined();
+  });
+  test("should be able to render a main", () => {
+    render(<HomePage />);
+
+    expect(screen.getAllByText("Em desenvolvimento...")).toBeDefined();
+  });
+  test("should be able to render a footer", () => {
+    render(<HomePage />);
+
+    expect(screen.getAllByText("A&T Desenvolvimento Web")).toBeDefined();
   });
 });
