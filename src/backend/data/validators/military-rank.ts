9865183980b43cfb3b82d3fc5e3866e1e42dff9b
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-06-13 08:28:11 -0300

    feat: ensure that AddMilitaryRankService throws if no abbreviatedName is provided

--- src/backend/data/validators/military-rank.ts
+++ src/backend/data/validators/military-rank.ts
@@ -3,18 +3,28 @@ import { missingParamError } from "../helpers";
 
 export class MilitaryRankValidator {
   private order: number;
+  private abbreviatedName: string;
 
   constructor() {
     this.order = 0;
+    this.abbreviatedName = "";
   }
 
   private setOrder = (order: number): void => {
     this.order = order;
   };
 
+  private setAbbreviatedName = (abbreviatedName: string): void => {
+    this.abbreviatedName = abbreviatedName;
+  };
+
   private validateProps = async (): Promise<void> => {
     if (!this.order) {
       throw missingParamError("ordem");
+    }
+
+    if (!this.abbreviatedName) {
+      throw missingParamError("nome abreviado");
     }
   };
 
@@ -22,6 +32,7 @@ export class MilitaryRankValidator {
     props: MilitaryRankProps
   ): Promise<void> => {
     this.setOrder(props.order);
+    this.setAbbreviatedName(props.abbreviatedName);
 
     await this.validateProps();
   };
