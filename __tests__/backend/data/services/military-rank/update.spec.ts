97bc6b072efb8da033f6098f220ee0c45ec3b550
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-06-16 11:12:24 -0300

    test: ensure that UpdateMilitaryRankService throws if no id is provided

--- __tests__/backend/data/services/military-rank/update.spec.ts
+++ __tests__/backend/data/services/military-rank/update.spec.ts
@@ -1,6 +1,11 @@
-import { MilitaryRankInMemoryRepository } from "@/../__mocks__";
+import {
+  IdValidatorStub,
+  MilitaryRankInMemoryRepository,
+} from "@/../__mocks__";
+import { missingParamError } from "@/backend/data/helpers";
 import { MilitaryRankRepository } from "@/backend/data/repositories";
 import { UpdateMilitaryRankService } from "@/backend/data/services";
+import { MilitaryRankValidator } from "@/backend/data/validators";
 import { describe, expect, test } from "vitest";
 
 interface SutResponse {
@@ -10,7 +15,9 @@ interface SutResponse {
 
 const makeSut = (): SutResponse => {
   const repository = new MilitaryRankInMemoryRepository();
-  const sut = new UpdateMilitaryRankService(repository);
+  const idValidator = new IdValidatorStub();
+  const validator = new MilitaryRankValidator({ idValidator, repository });
+  const sut = new UpdateMilitaryRankService({ repository, validator });
 
   return { repository, sut };
 };
@@ -27,5 +34,14 @@ describe("UpdateMilitaryRankService", () => {
     await expect(
       sut.update({ id, order: 2, abbreviatedName: "TC" })
     ).resolves.not.toThrow();
+  });
+
+  test("should be throws if no id is provided", async () => {
+    const { sut } = makeSut();
+
+    await expect(
+      //@ts-expect-error
+      sut.update({ order: 2, abbreviatedName: "TC" })
+    ).rejects.toThrow(missingParamError("ID"));
   });
 });
