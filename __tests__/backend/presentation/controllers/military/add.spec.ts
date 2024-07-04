b25837398c4074a4743e4b98cbbcc842a22040b8
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-07-04 09:46:50 -0300

    test: ensure that AddMilitaryController return 201 on success

---
+++ __tests__/backend/presentation/controllers/military/add.spec.ts
@@ -0,0 +1,62 @@
+import {
+  MilitaryInMemoryRepository,
+  MilitaryRankInMemoryRepository,
+} from "@/../__mocks__";
+import {
+  MilitaryRankRepository,
+  MilitaryRepository,
+} from "@/backend/data/repositories";
+import { AddMilitaryService } from "@/backend/data/services";
+import { MilitaryProps } from "@/backend/domain/entities";
+import { AddMilitaryController } from "@/backend/presentation/controllers";
+import { HttpRequest, HttpResponse } from "@/backend/presentation/protocols";
+import { describe, expect, test } from "vitest";
+
+interface SutResponse {
+  militaryRepository: MilitaryRepository;
+  militaryRankRepository: MilitaryRankRepository;
+  sut: AddMilitaryController;
+}
+
+const makeSut = (): SutResponse => {
+  const militaryRankRepository: MilitaryRankRepository =
+    new MilitaryRankInMemoryRepository();
+  const militaryRepository: MilitaryRepository = new MilitaryInMemoryRepository(
+    militaryRankRepository
+  );
+  const addMilitaryService = new AddMilitaryService({
+    repository: militaryRepository,
+  });
+
+  const sut = new AddMilitaryController(addMilitaryService);
+
+  return { militaryRepository, militaryRankRepository, sut };
+};
+
+describe("AddMilitaryController", () => {
+  test("should be return 201 on success", async () => {
+    const { militaryRankRepository, sut } = makeSut();
+
+    await militaryRankRepository.add({
+      order: 1,
+      abbreviatedName: "Cel",
+    });
+    const militaryRank =
+      await militaryRankRepository.getByAbbreviatedName("Cel");
+    const militaryRankId = militaryRank?.id || "";
+
+    const httpRequest: HttpRequest<MilitaryProps> = {
+      body: {
+        militaryRankId,
+        rg: 1,
+        name: "any-name",
+        role: "Usuário",
+        password: "any-password",
+      },
+    };
+
+    const httpResponse: HttpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(201);
+  });
+});
