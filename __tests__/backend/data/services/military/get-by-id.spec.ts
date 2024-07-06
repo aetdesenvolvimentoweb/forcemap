9cba18641bf23d5f955963bbbbb63e587586767f
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-07-06 08:30:46 -0300

    test: ensure that GetMilitaryByIdService to get a military by id

---
+++ __tests__/backend/data/services/military/get-by-id.spec.ts
@@ -0,0 +1,51 @@
+import {
+  MilitaryInMemoryRepository,
+  MilitaryRankInMemoryRepository,
+} from "@/../__mocks__";
+import {
+  MilitaryRankRepository,
+  MilitaryRepository,
+} from "@/backend/data/repositories";
+import { GetMilitaryByIdService } from "@/backend/data/services";
+import { describe, expect, test } from "vitest";
+
+interface SutResponse {
+  militaryRepository: MilitaryRepository;
+  militaryRankRepository: MilitaryRankRepository;
+  sut: GetMilitaryByIdService;
+}
+
+const makeSut = (): SutResponse => {
+  const militaryRankRepository = new MilitaryRankInMemoryRepository();
+  const militaryRepository = new MilitaryInMemoryRepository(
+    militaryRankRepository
+  );
+  const sut = new GetMilitaryByIdService({
+    repository: militaryRepository,
+  });
+
+  return { militaryRepository, militaryRankRepository, sut };
+};
+
+describe("GetMilitaryByIdService", () => {
+  test("should be able to get a military by id", async () => {
+    const { militaryRepository, militaryRankRepository, sut } = makeSut();
+
+    await militaryRankRepository.add({ order: 1, abbreviatedName: "Cel" });
+    const militaryRank =
+      await militaryRankRepository.getByAbbreviatedName("Cel");
+    const militaryRankId = militaryRank?.id || "";
+
+    await militaryRepository.add({
+      militaryRankId,
+      rg: 1,
+      name: "any-name",
+      role: "Usuário",
+      password: "any-password",
+    });
+    const military = await militaryRepository.getByRg(1);
+    const id = military?.id || "";
+
+    await expect(sut.getById(id)).resolves.not.toThrow();
+  });
+});
