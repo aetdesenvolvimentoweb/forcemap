ec73c460eae0513bdc4b0ed6411463411ab65438
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-07-08 17:15:12 -0300

    test: ensure that UpdateMilitaryProfileService throws if no name is provided

--- __tests__/backend/data/services/military/update-profile.spec.ts
+++ __tests__/backend/data/services/military/update-profile.spec.ts
@@ -286,4 +286,29 @@ describe("UpdateMilitaryProfileService", () => {
       })
     ).rejects.toThrow(duplicatedKeyError("RG"));
   });
+
+  test("should be throws if no name is provided", async () => {
+    const { militaryRepository, militaryRankRepository, sut } = makeSut();
+
+    await militaryRankRepository.add({ order: 1, abbreviatedName: "Cel" });
+    const militaryRank =
+      await militaryRankRepository.getByAbbreviatedName("Cel");
+    const militaryRankId = militaryRank?.id || "";
+
+    await militaryRepository.add({
+      militaryRankId,
+      rg: 1,
+      name: "any-name",
+      password: "any-password",
+      role: "Usuário",
+    });
+
+    const military = await militaryRepository.getByRg(1);
+    const id = military?.id || "";
+
+    await expect(
+      // @ts-expect-error
+      sut.updateProfile({ id, militaryRankId, rg: 2 })
+    ).rejects.toThrow(missingParamError("nome"));
+  });
 });
