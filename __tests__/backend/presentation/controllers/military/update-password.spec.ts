2a5e8333258e0f082853c0eec98e7d9005056366
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-07-09 11:04:51 -0300

    test: ensure that UpdateMilitaryPasswordController return 400 on current password no matches

--- __tests__/backend/presentation/controllers/military/update-password.spec.ts
+++ __tests__/backend/presentation/controllers/military/update-password.spec.ts
@@ -228,4 +228,42 @@ describe("UpdateMilitaryPasswordController", () => {
       invalidParamError("senha atual").message
     );
   });
+
+  test("should be return 400 on current password no matches", async () => {
+    const { militaryRankRepository, militaryRepository, idValidator, sut } =
+      makeSut();
+
+    await militaryRankRepository.add({
+      order: 1,
+      abbreviatedName: "Cel",
+    });
+    const militaryRank =
+      await militaryRankRepository.getByAbbreviatedName("Cel");
+    const militaryRankId = militaryRank?.id || "";
+
+    await militaryRepository.add({
+      militaryRankId,
+      rg: 1,
+      name: "any-name",
+      role: "Usuário",
+      password: "current-password",
+    });
+    const military = await militaryRepository.getByRg(1);
+    const id = military?.id || "";
+
+    const httpRequest: HttpRequest<Omit<UpdateMilitaryPasswordProps, "id">> = {
+      body: {
+        currentPassword: "wrong-password",
+        newPassword: "any-new-password",
+      },
+      params: { id },
+    };
+
+    const httpResponse: HttpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body.errorMessage).toEqual(
+      invalidParamError("senha atual").message
+    );
+  });
 });
