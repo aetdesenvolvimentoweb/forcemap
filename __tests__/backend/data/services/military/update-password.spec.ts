21ce91dd7b8477d0fa91eade1823b04f41111d3f
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-07-09 10:35:23 -0300

    test: ensure that UpdateMilitaryPasswordService throws if no current password is provided

--- __tests__/backend/data/services/military/update-password.spec.ts
+++ __tests__/backend/data/services/military/update-password.spec.ts
@@ -116,4 +116,29 @@ describe("UpdateMilitaryPasswordService", () => {
 
     mockUnregisteredId.mockRestore();
   });
+
+  test("should be throws if no current password is provided", async () => {
+    const { militaryRepository, militaryRankRepository, sut } = makeSut();
+
+    await militaryRankRepository.add({ order: 1, abbreviatedName: "Cel" });
+    const militaryRank =
+      await militaryRankRepository.getByAbbreviatedName("Cel");
+    const militaryRankId = militaryRank?.id || "";
+
+    await militaryRepository.add({
+      militaryRankId,
+      rg: 1,
+      name: "any-name",
+      password: "any-password",
+      role: "Usuário",
+    });
+
+    const military = await militaryRepository.getByRg(1);
+    const id = military?.id || "";
+
+    await expect(
+      // @ts-expect-error
+      sut.updatePassword({ id, newPassword: "another-password" })
+    ).rejects.toThrow(missingParamError("senha atual"));
+  });
 });
