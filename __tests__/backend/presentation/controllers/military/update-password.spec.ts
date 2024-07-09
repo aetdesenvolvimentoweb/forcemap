0eca1825f4aa299c4f6a6d0f843e4e524e5a6bb7
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-07-09 11:35:36 -0300

    test: ensure that UpdateMilitaryPasswordController return 500 on server error

--- __tests__/backend/presentation/controllers/military/update-password.spec.ts
+++ __tests__/backend/presentation/controllers/military/update-password.spec.ts
@@ -17,6 +17,7 @@ import { MilitaryValidator } from "@/backend/data/validators";
 import { UpdateMilitaryPasswordProps } from "@/backend/domain/entities";
 import { IdValidator } from "@/backend/domain/usecases";
 import { UpdateMilitaryPasswordController } from "@/backend/presentation/controllers";
+import { serverError } from "@/backend/presentation/helpers";
 import { HttpRequest, HttpResponse } from "@/backend/presentation/protocols";
 import { describe, expect, test, vi } from "vitest";
 
@@ -340,5 +341,47 @@ describe("UpdateMilitaryPasswordController", () => {
     expect(httpResponse.body.errorMessage).toEqual(
       invalidParamError("nova senha").message
     );
+  });
+
+  test("should be return 500 on server error", async () => {
+    const { militaryRankRepository, militaryRepository, sut } = makeSut();
+
+    const mockServerError = vi.spyOn(militaryRepository, "updatePassword");
+    mockServerError.mockRejectedValueOnce(new Error());
+
+    await militaryRankRepository.add({
+      order: 1,
+      abbreviatedName: "Cel",
+    });
+    const militaryRank =
+      await militaryRankRepository.getByAbbreviatedName("Cel");
+    const militaryRankId = militaryRank?.id || "";
+
+    await militaryRepository.add({
+      militaryRankId,
+      rg: 1,
+      name: "any-name",
+      role: "Usuário",
+      password: "current-password",
+    });
+    const military = await militaryRepository.getByRg(1);
+    const id = military?.id || "";
+
+    const httpRequest: HttpRequest<Omit<UpdateMilitaryPasswordProps, "id">> = {
+      body: {
+        currentPassword: "current-password",
+        newPassword: "any-new-password",
+      },
+      params: { id },
+    };
+
+    const httpResponse: HttpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body.errorMessage).toEqual(
+      serverError().body.errorMessage
+    );
+
+    mockServerError.mockRestore();
   });
 });
