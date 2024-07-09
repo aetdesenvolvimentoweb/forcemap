96214719cc0b8a8b87735e7b5e05a83452de2c43
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-07-09 10:37:31 -0300

    test: ensure that UpdateMilitaryPasswordController return 400 on missing current password

--- __tests__/backend/presentation/controllers/military/update-password.spec.ts
+++ __tests__/backend/presentation/controllers/military/update-password.spec.ts
@@ -153,4 +153,41 @@ describe("UpdateMilitaryPasswordController", () => {
 
     mockUnregisteredId.mockRestore();
   });
+
+  test("should be return 400 on missing current password", async () => {
+    const { militaryRankRepository, militaryRepository, sut } = makeSut();
+
+    await militaryRankRepository.add({
+      order: 1,
+      abbreviatedName: "Cel",
+    });
+    const militaryRank =
+      await militaryRankRepository.getByAbbreviatedName("Cel");
+    const militaryRankId = militaryRank?.id || "";
+
+    await militaryRepository.add({
+      militaryRankId,
+      rg: 1,
+      name: "any-name",
+      role: "Usuário",
+      password: "current-password",
+    });
+    const military = await militaryRepository.getByRg(1);
+    const id = military?.id || "";
+
+    const httpRequest: HttpRequest<Omit<UpdateMilitaryPasswordProps, "id">> = {
+      // @ts-expect-error
+      body: {
+        newPassword: "any-new-password",
+      },
+      params: { id },
+    };
+
+    const httpResponse: HttpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body.errorMessage).toEqual(
+      missingParamError("senha atual").message
+    );
+  });
 });
