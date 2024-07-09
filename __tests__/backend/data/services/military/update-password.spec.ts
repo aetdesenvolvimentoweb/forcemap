8e4224a45e47bc0c3bf6c05a7852413a0f6f2366
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2024-07-09 10:19:03 -0300

    test: ensure that UpdateMilitaryPasswordService throws if invalid ID is provided

--- __tests__/backend/data/services/military/update-password.spec.ts
+++ __tests__/backend/data/services/military/update-password.spec.ts
@@ -3,18 +3,20 @@ import {
   MilitaryInMemoryRepository,
   MilitaryRankInMemoryRepository,
 } from "@/../__mocks__";
-import { missingParamError } from "@/backend/data/helpers";
+import { invalidParamError, missingParamError } from "@/backend/data/helpers";
 import {
   MilitaryRankRepository,
   MilitaryRepository,
 } from "@/backend/data/repositories";
 import { UpdateMilitaryPasswordService } from "@/backend/data/services";
 import { MilitaryValidator } from "@/backend/data/validators";
-import { describe, expect, test } from "vitest";
+import { IdValidator } from "@/backend/domain/usecases";
+import { describe, expect, test, vi } from "vitest";
 
 interface SutResponse {
   militaryRepository: MilitaryRepository;
   militaryRankRepository: MilitaryRankRepository;
+  idValidator: IdValidator;
   sut: UpdateMilitaryPasswordService;
 }
 
@@ -34,7 +36,7 @@ const makeSut = (): SutResponse => {
     validator,
   });
 
-  return { militaryRepository, militaryRankRepository, sut };
+  return { militaryRepository, militaryRankRepository, idValidator, sut };
 };
 
 describe("UpdateMilitaryPasswordService", () => {
@@ -65,7 +67,7 @@ describe("UpdateMilitaryPasswordService", () => {
     ).resolves.not.toThrow();
   });
 
-  test("should be throws if no id is provided", async () => {
+  test("should be throws if no ID is provided", async () => {
     const { sut } = makeSut();
 
     await expect(
@@ -75,5 +77,22 @@ describe("UpdateMilitaryPasswordService", () => {
         newPassword: "another-password",
       })
     ).rejects.toThrow(missingParamError("ID"));
+  });
+
+  test("should be throws if invalid ID is provided", async () => {
+    const { idValidator, sut } = makeSut();
+
+    const mockInvalidId = vi.spyOn(idValidator, "isValid");
+    mockInvalidId.mockReturnValueOnce(false);
+
+    await expect(
+      sut.updatePassword({
+        id: "invalid-id",
+        currentPassword: "any-password",
+        newPassword: "another-password",
+      })
+    ).rejects.toThrow(invalidParamError("ID"));
+
+    mockInvalidId.mockRestore();
   });
 });
