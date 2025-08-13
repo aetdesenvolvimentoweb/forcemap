d0a7a31916d44b4c4ba5c5320af7068bbd1915ad
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-08-13 10:25:07 -0300

    chore: configure pre-push in husky

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,14 @@ const config: Config = {
   collectCoverage: false,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   passWithNoTests: true,
 };
 
