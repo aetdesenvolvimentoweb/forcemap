c8844a1005f21bcec48174dd1ced78bed62e831f
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-08-13 14:50:25 -0300

    fix: improve paths configuration

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,26 @@ const config: Config = {
     },
   },
   passWithNoTests: true,
+  preset: "ts-jest/presets/default-esm",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  roots: ["<rootDir>/src", "<rootDir>/__tests__"],
+  testMatch: ["**/__tests__/**/*.spec.ts", "**/__tests__/**/*.test.ts"],
+  moduleNameMapper: {
+    "^@domain/(.*)$": "<rootDir>/src/domain/$1",
+    "^@application/(.*)$": "<rootDir>/src/application/$1",
+    "^@infra/(.*)$": "<rootDir>/src/infra/$1",
+    "^@presentation/(.*)$": "<rootDir>/src/presentation/$1",
+    "^@tests/(.*)$": "<rootDir>/__tests__/$1",
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
 
 export default config;
