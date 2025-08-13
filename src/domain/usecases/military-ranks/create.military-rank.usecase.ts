24d9cb9a5505d1f8d6494020ac052e4fdd80519a
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-08-13 14:04:21 -0300

    refactor: implements typescript paths

--- src/domain/usecases/military-ranks/create.military-rank.usecase.ts
+++ src/domain/usecases/military-ranks/create.military-rank.usecase.ts
@@ -1,5 +1,5 @@
-import { CreateMilitaryRankDTO } from "../../dtos";
-import { MilitaryRank } from "../../entities";
+import { CreateMilitaryRankDTO } from "@domain/dtos";
+import { MilitaryRank } from "@domain/entities";
 
 export interface CreateMilitaryRankUseCase {
   create(data: CreateMilitaryRankDTO): Promise<MilitaryRank>;
