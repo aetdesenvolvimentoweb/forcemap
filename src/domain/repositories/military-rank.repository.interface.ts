9cc2a86baad414fea22a918671a2d36baa785f74
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-08-16 17:34:30 -0300

    feat: add lista all use case to repository interface

--- src/domain/repositories/military-rank.repository.interface.ts
+++ src/domain/repositories/military-rank.repository.interface.ts
@@ -1,8 +1,12 @@
-import type { CreateMilitaryRankInputDTO } from "@domain/dtos";
+import type {
+  CreateMilitaryRankInputDTO,
+  MilitaryRankListItemDTO,
+} from "@domain/dtos";
 import type { MilitaryRank } from "@domain/entities";
 
 export interface MilitaryRankRepository {
   create(data: CreateMilitaryRankInputDTO): Promise<void>;
   findByAbbreviation(abbreviation: string): Promise<MilitaryRank | null>;
   findByOrder(order: number): Promise<MilitaryRank | null>;
+  listAll(): Promise<MilitaryRankListItemDTO[]>;
 }
