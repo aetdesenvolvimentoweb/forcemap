1f974eaf5dbc304877e7bf270da8ec570f584a43
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-08-19 16:06:32 -0300

    feat: create dto input to military rank update

--- src/domain/dtos/military-rank.dtos.ts
+++ src/domain/dtos/military-rank.dtos.ts
@@ -2,3 +2,8 @@ export interface CreateMilitaryRankInputDTO {
   abbreviation: string;
   order: number;
 }
+
+export interface UpdateMilitaryRankInputDTO {
+  abbreviation?: string;
+  order?: number;
+}
