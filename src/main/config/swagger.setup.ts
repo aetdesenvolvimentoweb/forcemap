e1bcd82b6a055470eeabcb0baa280beade37d1e9
A & T Desenvolvimento Web <101463037+aetdesenvolvimentoweb@users.noreply.github.com>
2025-08-20 15:45:36 +0000

    fix: swagger dont print

--- src/main/config/swagger.setup.ts
+++ src/main/config/swagger.setup.ts
@@ -4,11 +4,12 @@ import swaggerUi from "swagger-ui-express";
 import { swaggerConfig } from "../docs/swagger.config";
 
 export const setupSwagger = (app: Express): void => {
-  // Configurações adicionais para o Swagger UI
+  // Configurações do Swagger UI
   const options: swaggerUi.SwaggerUiOptions = {
     customCss: ".swagger-ui .topbar { display: none }",
     customSiteTitle: "ForceMap API Documentation",
     swaggerOptions: {
+      url: "/api-docs/swagger.json", // Adicione esta linha
       persistAuthorization: true,
       tryItOutEnabled: true,
       displayRequestDuration: true,
@@ -17,6 +18,13 @@ export const setupSwagger = (app: Express): void => {
     explorer: true,
   };
 
+  // Rota para servir o JSON do Swagger
+  app.get("/api-docs/swagger.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerConfig);
+  });
+
+  // Setup do Swagger UI
   app.use("/api-docs", swaggerUi.serve);
   app.get("/api-docs", swaggerUi.setup(swaggerConfig, options));
 };
