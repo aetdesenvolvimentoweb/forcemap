ce0fd9226b2a35932493dde07ddd153908252e8b
A & T Desenvolvimento Web <101463037+aetdesenvolvimentoweb@users.noreply.github.com>
2025-08-20 15:39:19 +0000

    fix: swagger working in vercel

--- src/main/config/swagger.setup.ts
+++ src/main/config/swagger.setup.ts
@@ -1,7 +1,22 @@
-import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
+import swaggerUi from "swagger-ui-express";
+
 import { swaggerConfig } from "../docs/swagger.config";
 
 export const setupSwagger = (app: Express): void => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
+  // Configurações adicionais para o Swagger UI
+  const options: swaggerUi.SwaggerUiOptions = {
+    customCss: ".swagger-ui .topbar { display: none }",
+    customSiteTitle: "ForceMap API Documentation",
+    swaggerOptions: {
+      persistAuthorization: true,
+      tryItOutEnabled: true,
+      displayRequestDuration: true,
+      filter: true,
+    },
+    explorer: true,
+  };
+
+  app.use("/api-docs", swaggerUi.serve);
+  app.get("/api-docs", swaggerUi.setup(swaggerConfig, options));
 };
