fc644ee68c62b2f58ca0de0c359e94db30470df1
A & T Desenvolvimento Web <101463037+aetdesenvolvimentoweb@users.noreply.github.com>
2025-08-27 14:35:49 +0000

    refactor: change server start strategy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import "module-alias/register";
 
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import { createServer } from "http";
 
 import app from "./main/server";
 
-export default (req: VercelRequest, res: VercelResponse) => {
-  app(req, res);
-};
+const server = createServer(app);
+
+export default server;
