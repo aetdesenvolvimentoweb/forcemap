e90c1734904ff91c0d326764addffc2d5ff5c345
A & T Desenvolvimento Web <101463037+aetdesenvolvimentoweb@users.noreply.github.com>
2025-08-27 14:40:31 +0000

    refactor: change server starter strategy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import "module-alias/register";
 
-import { createServer } from "http";
-
 import app from "./main/server";
 
-const server = createServer(app);
-
-export default server;
+export default app;
