19286bea83618b1c296bfa86ebe4cad76ffeeb3a
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-08-28 15:29:28 -0300

    feat: implements list by id Military Rank in application layer

---
+++ src/application/services/military-rank/list-by-id.military-rank.service.ts
@@ -0,0 +1,53 @@
+import { MilitaryRank } from "src/domain/entities";
+
+import { MilitaryRankRepository } from "../../../domain/repositories";
+import { ListByIdMilitaryRankUseCase } from "../../../domain/use-cases";
+import {
+  IdSanitizerProtocol,
+  IdValidatorProtocol,
+  LoggerProtocol,
+} from "../../protocols";
+
+interface ListByIdMilitaryRankServiceProps {
+  militaryRankRepository: MilitaryRankRepository;
+  sanitizer: IdSanitizerProtocol;
+  validator: IdValidatorProtocol;
+  logger: LoggerProtocol;
+}
+
+export class ListByIdMilitaryRankService
+  implements ListByIdMilitaryRankUseCase
+{
+  private readonly logger: LoggerProtocol;
+  private readonly props: ListByIdMilitaryRankServiceProps;
+
+  constructor(props: ListByIdMilitaryRankServiceProps) {
+    this.props = props;
+    this.logger = props.logger;
+  }
+
+  public readonly listById = async (
+    id: string,
+  ): Promise<MilitaryRank | null> => {
+    const { militaryRankRepository, sanitizer, validator } = this.props;
+    this.logger.info("ListByIdMilitaryRankService.listById called", {
+      input: id,
+    });
+
+    try {
+      const sanitizedId = sanitizer.sanitize(id);
+      this.logger.info("Sanitized ID", { sanitizedId });
+      await validator.validate(sanitizedId);
+      this.logger.info("Validation passed", { sanitizedId });
+      const militaryRank = await militaryRankRepository.listById(sanitizedId);
+      this.logger.info("Military rank retrieved successfully", {
+        sanitizedId,
+        militaryRank,
+      });
+      return militaryRank;
+    } catch (error) {
+      this.logger.error("Error retrieving military rank", { input: id, error });
+      throw error;
+    }
+  };
+}
