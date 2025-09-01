a84ee3539e10c778cee916ba404883a0eea078dc
A & T Desenvolvimento Web <101463037+aetdesenvolvimentoweb@users.noreply.github.com>
2025-09-01 15:19:36 +0000

    refactor: clear configurations

--- index.ts
+++
@@ -1,7 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-import app from "./src/main/server";
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  app(req, res);
-};
