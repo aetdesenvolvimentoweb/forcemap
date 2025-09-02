88a39b610fd1ffc4ed78902bd58aa656f3006c3d
André David dos Santos <aetdesenvolvimentoweb@gmail.com>
2025-09-02 16:32:38 -0300

    refactor: correct uniqueness method from findByRg to findByName

--- src/domain/repositories/vehicle.repository.ts
+++ src/domain/repositories/vehicle.repository.ts
@@ -4,9 +4,8 @@ import { Vehicle } from "../entities";
 export interface VehicleRepository {
   create(data: VehicleInputDTO): Promise<void>;
   delete(id: string): Promise<void>;
-  findByAbbreviation(abbreviation: string): Promise<Vehicle | null>;
   findById(id: string): Promise<Vehicle | null>;
-  findByOrder(order: number): Promise<Vehicle | null>;
+  findByName(name: string): Promise<Vehicle | null>;
   listAll(): Promise<Vehicle[]>;
   update(id: string, data: VehicleInputDTO): Promise<void>;
 }
